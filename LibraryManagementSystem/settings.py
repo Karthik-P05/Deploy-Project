8e2125f245770187edf7856410fefbf9294fcfbd
Karthik P <karthikp11.98@gmail.com>
2025-07-29 00:49:15 +0530

    update

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -12,6 +12,17 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://library-management-system.up.railway.app"
+]
+
+ALLOWED_HOSTS = [
+    "library-management-system.up.railway.app",
+    "127.0.0.1",
+    "localhost"
+]
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
